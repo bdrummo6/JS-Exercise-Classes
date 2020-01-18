e1982509930c5238a5be059dad14d39dc3f412f4
Bradley Drummonds <47236494+bdrummo6@users.noreply.github.com>
2020-01-17 19:19:55 -0500

    Update index.js

--- index.js
+++ index.js
@@ -109,7 +109,8 @@ class Lambdasian {
     this.name = attributes.name;
     this.age = attributes.age;
     this.location = attributes.location;
-  } speak() {
+  }
+  speak() {
     return "Hello my name is " + this.name + ", I am from " + this.location;
   }
 }
@@ -128,8 +129,11 @@ class Lambdasian {
         + `demo` receives a `subject` string as an argument and returns the phrase 'Today we are learning about {subject}' where subject is the param passed in.
         + `grade` receives a `student` object and a `subject` string as arguments and returns '{student.name} receives a perfect score on {subject}'
 */
-class Instructor {
-
+class Instructor extends Lambdasian {
+  constructor (instructAtts) {
+    super(instuctAtts);
+    this.isChild = instructAtts.isChild;
+  }
 }
 
 /*
